edaa9cc8675c8a052c34352c7c46b986fc6990af
Deekline <nicholas.yemets@gmail.com>
2025-10-02 16:34:38 +0200

    Test

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ use std::net::TcpListener;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to get port");
+
     run(listener)?.await
 }
