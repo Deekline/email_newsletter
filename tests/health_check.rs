1006de37908cc8f146162a0f2e54390903e4b801
Deekline <nicholas.yemets@gmail.com>
2025-10-13 22:13:51 +0200

    Fix tests with telemetry. Add sinking for logs

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,25 @@
 use email_newsletter::{
     configuration::{DatabaseSettings, get_configuration},
     startup::run,
+    telemetry::{get_subsciber, init_subscriber},
 };
+use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use uuid::Uuid;
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subsciber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subsciber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    }
+});
 
 pub struct TestApp {
     pub address: String,
@@ -81,6 +96,8 @@ async fn invalid_subscibe_form() {
 }
 
 async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
