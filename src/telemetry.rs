54d5c2d00127c6b895e6fb063302f983035be73a
Deekline <nicholas.yemets@gmail.com>
2025-10-13 21:59:19 +0200

    telemetry refactoring

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -4,8 +4,9 @@ use tracing_log::LogTracer;
 use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt};
 
 pub fn get_subsciber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("email_newsletter".into(), std::io::stdout);
+    let env_filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
+    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
 
     Registry::default()
         .with(env_filter)
